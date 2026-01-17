370e748e1c2ac88a65375c35424639ca906c0c84
Romi García <miagar11@outlook.es>
2026-01-17 16:12:57 -0600

    Se agrega la clase Ticket para hacerel ticket de compra

---
+++ java/src/edu/mia/garcia/actividad1/data/Ticket.java
@@ -0,0 +1,39 @@
+package edu.mia.garcia.actividad1.data;
+
+import java.util.List;
+
+public class Ticket {
+
+    private String nombreCliente;
+    private String fecha;
+    private List<Auto> autos;
+    private int total;
+
+    public Ticket(String nombreCliente, String fecha, List<Auto> autos) {
+        this.nombreCliente = nombreCliente;
+        this.fecha = fecha;
+        this.autos = autos;
+        calcularTotal();
+    }
+
+    private void calcularTotal() {
+        total = 0;
+        for (Auto auto : autos) {
+            total += auto.getPrecio();
+        }
+    }
+
+    public void mostrarTicket() {
+        System.out.println("-----TICKET-----");
+        System.out.println("Cliente: " + nombreCliente);
+        System.out.println("Fecha: " + fecha);
+        System.out.println("Autos comprados:");
+
+        for (Auto auto : autos) {
+            System.out.println("- " + auto.getModelo() + " $" + auto.getPrecio());
+        }
+
+        System.out.println("TOTAL A PAGAR: $" + total);
+    }
+}
+
